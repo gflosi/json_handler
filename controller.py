bb277a56ff05dc31081c9e5fc8e18d43fcdeae49
Guilherme Flosi <glimaflosi@gmail.com>
2019-02-19 00:49:29 -0300

    att

--- controller.py
+++ controller.py
@@ -4,4 +4,7 @@ import json
 class Controller:
     def __init__(self):
         data = json.loads(open('textos.txt').read())
-        print(data["SUBJECT"]["EN"])
+        for key, value in data["SUBJECT"].items():
+            print(key)
+
+        # print(data[0]["EN"])
