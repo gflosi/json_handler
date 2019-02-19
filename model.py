69cab4ea46fc0b37e3a8ac2d9cb4417de0f315eb
Guilherme <guilherme.flosi@pmweb.com.br>
2019-02-19 20:08:57 -0300

    Versão 1.0.0 - Recursividade em Produção

--- model.py
+++ model.py
@@ -7,8 +7,6 @@ class Model:
     def __init__(self):
         self.data_json = json.loads(open('textos.txt').read())
 
-        self.data_json['TABLAITINERARIO']['tmp'] = ""
-
         self.d = {}
         self.dic_tmp = {}
 
@@ -16,16 +14,11 @@ class Model:
 
         self.dic_tmp.update(self.create_node_tmp(json_data))
 
-        print ("FINAAAL")
-        print(self.dic_tmp)
-        #print(self.data_json)
+        self.data_json['TABLAITINERARIO']['tmp'] = self.dic_tmp
 
-        # self.data_json['TABLAITINERARIO']['TK'] = {"ok":"a", "ok2":"b"}
+        print(self.data_json)
 
     def create_node_tmp(self, node):
-
-        #print(node)
-
         d = {}
         dic_tmp = {}
 
@@ -33,18 +26,8 @@ class Model:
             try:
                 if (len(node.items()) > 0):
                     dic_tmp.update({key: self.create_node_tmp(node[key])}.copy())
-                    #(d)
-                    print (dic_tmp)
-                    #return self.create_node_tmp(node[key])
             except:
                 d = {key: "tmp"}.copy()
                 dic_tmp.update(d)
-                print (dic_tmp)
 
         return dic_tmp
-        #dic_tmp.update(d)
-
-
-        #return dic_tmp
-
-        #return node
