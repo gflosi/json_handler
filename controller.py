b65939b8ec4fa3d80014bb04da5b7cef5daabdac
Guilherme Flosi <glimaflosi@gmail.com>
2019-02-18 23:59:56 -0300

    att

--- controller.py
+++ controller.py
@@ -1,2 +1,11 @@
 import json
+from pprint import pprint
 
+
+class Controller:
+
+    def __init__(self):
+        with open('textos.json') as f:
+            data = json.load(f)
+
+        print(data)
