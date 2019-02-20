10291db3859e24d8c5c1bfba153c5d1d02f441f3
Guilherme <guilherme.flosi@pmweb.com.br>
2019-02-20 10:39:07 -0300

    Prevendo quando o nó é uma string

--- model.py
+++ model.py
@@ -1,33 +1,32 @@
 import json
-import copy
-import yaml
 
 
 class Model:
     def __init__(self):
         self.data_json = json.loads(open('textos.txt').read())
 
-        self.d = {}
+        # self.d = {}
         self.dic_tmp = {}
 
-        json_data = self.data_json['TABLAITINERARIO']['ES']
+        json_data = self.data_json['TEXTOITINERARIO']['ES']
 
-        self.dic_tmp.update(self.create_node_tmp(json_data))
-
-        self.data_json['TABLAITINERARIO']['tmp'] = self.dic_tmp
+        if type(json_data) == str:
+            self.data_json['TEXTOITINERARIO']['tmp'] = "tmp"
+        else:
+            self.dic_tmp.update(self.create_node_tmp(json_data))
+            self.data_json['TEXTOITINERARIO']['tmp'] = self.dic_tmp
 
         print(self.data_json)
 
     def create_node_tmp(self, node):
-        d = {}
-        dic_tmp = {}
+        array_aux = {}
 
         for key, value in node.items():
-            try:
-                if (len(node.items()) > 0):
-                    dic_tmp.update({key: self.create_node_tmp(node[key])}.copy())
-            except:
-                d = {key: "tmp"}.copy()
-                dic_tmp.update(d)
+           try:
+               if len(node.items()) > 0:
+                   array_aux.update({key: self.create_node_tmp(node[key])}.copy())
+           except:
+                node_aux = {key: "tmp"}.copy()
+                array_aux.update(node_aux)
 
-        return dic_tmp
+        return array_aux
